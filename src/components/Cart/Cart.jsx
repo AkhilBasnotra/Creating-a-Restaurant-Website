e8338e6ba989cab3a9cb688932d5871464c4ac4c
Akhil Basnotra <akhilbasnotra24@gmail.com>
2024-03-04 23:53:38 +0530

    added Cart component

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,27 @@
+import styles from "./Cart.module.css";
+
+const Cart = () => {
+  const cartItems = (
+    <ul className={styles["cart-items"]}>
+      {[{ id: "c1", name: "Akhil", amount: 2, price: 12.99 }].map((item) => (
+        <li key={item.id}>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <div>
+      {cartItems}
+      <div className={styles.total}>
+        <span>Total Amount</span>
+        <span>34.99</span>
+      </div>
+      <div className={styles.actions}>
+        <button className={styles["button--alt"]}>Close</button>
+        <button className={styles.button}></button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
