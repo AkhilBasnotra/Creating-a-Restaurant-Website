55bf987fb3c8050c454d8190e108c0080c8c282a
Akhil Basnotra <akhilbasnotra24@gmail.com>
2024-03-11 01:11:59 +0530

    added showCardHandler function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Card from "./components/UI/Card";
+import { useState, Fragment } from "react";
+import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
   return (
-    <div>
-      <Header />
-      <Meals />
-    </div>
+    <Fragment>
+      {cartIsShown && <Cart />}
+      <Header onShowCartHandler={showCartHandler} />
+
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
   );
 }
 
